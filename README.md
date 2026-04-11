e0b7b4a37e852fe879d56c167e5dc233c4fa2e63
Florian Hartung <60144801+florianhartung@users.noreply.github.com>
2026-04-11 20:08:02 +0200

    init: empty rust project

---
+++ README.md
@@ -0,0 +1,6 @@
+# OS from Scratch
+
+
+## References
+
+- [Writing an OS in Rust _by Philipp Oppermann_](https://os.phil-opp.com/)
---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "TODO";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    devshell.url = "github:numtide/devshell";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devshell,
+      utils,
+      rust-overlay,
+      treefmt-nix,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [
+          devshell.overlays.default
+          (import rust-overlay)
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-target = pkgs.pkgsStatic.stdenv.targetPlatform.rust.rustcTarget;
+        rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = [
+            rust-target
+          ];
+        };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+      in
+      {
+        git.hooks = {
+          enable = true;
+          pre-commit.text = "nix flake check";
+        };
+
+        devShells.default = (
+          pkgs.devshell.mkShell {
+            name = "dev";
+            packages = with pkgs; [
+              stdenv.cc
+              coreutils
+
+              rust-toolchain
+              rust-analyzer
+              cargo-expand
+              cargo-show-asm
+              cargo-outdated
+            ];
+          }
+        );
+
+        formatter = treefmtEval.config.build.wrapper;
+
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+      }
+    );
+  # });
+}

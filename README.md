f10957036985aa789b04ac51fba364b824f80d32
Florian Hartung <60144801+florianhartung@users.noreply.github.com>
2026-04-19 00:36:58 +0200

    build: add asm/md formatters

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # OS from Scratch
 
-
 ## References
 
 - [Writing an OS in Rust _by Philipp Oppermann_](https://os.phil-opp.com/)
--- treefmt.nix
+++ treefmt.nix
@@ -3,10 +3,11 @@
   # Used to find the project root
   projectRootFile = "flake.nix";
 
+  programs.mdformat.enable = true;
   programs.nixfmt.enable = true;
-  programs.taplo.enable = true; # toml
   programs.rustfmt = {
     enable = true;
     edition = "2024";
   };
+  programs.taplo.enable = true; # toml
 }
